b4e6c4218f6dcc7ba794c7cbb516fea9e845fa83
milanpanyik <milanpanyik@gmail.com>
2023-09-06 18:04:03 +0200

    added selecting functions

---
+++ script.js
@@ -0,0 +1,8 @@
+//This project is a Cards against humanity generator
+// Returning random number
+let generateRandomNumber = (num) => {
+    return Math.floor(Math.random() * num);
+};
+
+console.log(generateRandomNumber(5));
+
